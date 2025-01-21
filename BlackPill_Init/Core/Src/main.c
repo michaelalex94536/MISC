6d26990fefae8d48e2f37a3c5f7b3c05891dc7bd
Michael Alex <michaelalex94536@gmail.com>
2025-01-21 12:13:29 -0800

    Changed input button type

--- BlackPill_Init/Core/Src/main.c
+++ BlackPill_Init/Core/Src/main.c
@@ -237,7 +237,6 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : BTN_Pin */
   GPIO_InitStruct.Pin = BTN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(BTN_GPIO_Port, &GPIO_InitStruct);
